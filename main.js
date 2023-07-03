b3c48a68c0772d4fea9d5bf3c374f6c2fc5e3baf
GabiBarbera <gabriel.barberaa@gmail.com>
2023-07-03 09:53:04 -0300

    Ejercicios funciones

---
+++ main.js
@@ -0,0 +1,31 @@
+//  let nombre = "Gabriel"
+
+//   function saludar(nombre){
+//           let resultado = nombre
+//           return resultado
+//   }
+
+//   let retorno = saludar(nombre)
+
+//   console.log("Hola, buen dia " + retorno)
+
+
+// let numero1 = 6
+// let numero2 = 8
+
+// function multiplicar(a,b){
+//     let resultado = a*b
+//     console.log(resultado)
+// }
+
+// multiplicar(numero1,numero2)
+
+// function areaTriangulo(base,altura){
+//     let resultado = (base + altura) / 2
+//     console.log(resultado)
+// }
+
+// areaTriangulo(10,5)
+
+
+function perimetroTriangulo()
