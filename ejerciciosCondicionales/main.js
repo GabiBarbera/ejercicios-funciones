f9179baa166d82d43d7a16d0813a8bd7826d750b
GabiBarbera <gabriel.barberaa@gmail.com>
2023-07-03 23:31:18 -0300

    Ejercicios condicionales

--- ejerciciosCondicionales/main.js
+++ ejerciciosCondicionales/main.js
@@ -56,3 +56,57 @@
 // esMasChico(5,4,2)
 
 //Ejercicio 5
+
+// const persona1 = {
+//     nombre: "Gabriel",
+//     edad: 29,
+//     altura: 169
+// }
+
+// const persona2 = {
+//     nombre: "Bruno",
+//     edad: 23,
+//     altura: 180
+// }
+
+// console.log(persona1.altura < persona2.altura)
+// console.log(persona1.edad < persona2.edad)
+
+// function altura(personaUno, personaDos) {
+//     let alturaPersona1 = personaUno.altura
+//     let alturaPersona2 = personaDos.altura
+//     if (alturaPersona1 > alturaPersona2) {
+//         console.log(`La persona mas alta es ${personaUno.nombre}`)
+//     } else {
+//         console.log(`La persona mas alta es ${personaDos.nombre}`)
+//     }
+// }
+
+// altura(persona1, persona2)
+
+// function edad(personaUno, personaDos) {
+//     let edadPersona1 = personaUno.edad
+//     let edadPersona2 = personaDos.edad
+//     if (edadPersona1 > edadPersona2) {
+//         console.log(`La persona mas grande es ${personaUno.nombre}`)
+//     } else {
+//         console.log(`La persona mas grande es ${personaDos.nombre}`)
+//     }
+// }
+
+// edad(persona1, persona2)
+
+//Ejercicio 6
+
+// let edad = prompt("Ingrese su edad")
+// let altura = prompt("Ingrese su altura")
+// let vision = prompt("Ingrese su vision 0/10")
+
+// function licencia(edad, altura, vision) {
+//     if (edad < 18 || altura < 150 || vision < 8) {
+//         console.log("Permiso denegado")
+//     } else if (edad >= 18 && altura >= 150 && vision >= 8) {
+//         console.log("Permiso aceptado")
+//     }
+// }
+// licencia(edad, altura, vision)
