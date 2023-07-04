f07b944faabefc32f5af6e5c2d0bc10ae58476dc
GabiBarbera <gabriel.barberaa@gmail.com>
2023-07-03 21:56:51 -0300

    Ejercicios js

---
+++ ejerciciosCondicionales/main.js
@@ -0,0 +1,58 @@
+// Ejercicio 1
+
+//  function esMayor(num1,num2){
+//      if(num1 > num2){
+//          console.log("Es mayor")
+//      }else{
+//          console.log("Es menor")
+//      }
+//  }
+//  esMayor(92,9)
+//  esMayor(3,6)
+
+// Ejercicio 2
+
+// function sonIguales(num1,num2){
+// if(num1 == num2){
+//     console.log("Son iguales")
+// }else{
+//     console.log("No son iguales")
+// }
+// }
+
+// Ejercicio 3
+
+// sonIguales(5,5)
+// sonIguales(5,8)
+
+// function esMasGrande(num1,num2){
+//     if(num1 === num2){
+//         console.log("Son iguales")
+//     }else if(num1 = num2){
+//         console.log(`Es mas grande ` + num1)
+//     }else{
+//         console.log(`Es mas grande ` + num2)
+//     }
+// }
+
+// esMasGrande(5,5)
+// esMasGrande(5,4)
+// esMasGrande(6,4)
+
+//Ejercicio 4
+
+// function esMasChico(num1,num2,num3){
+//     if(num1 < num2 && num1 < num3){
+//         console.log("Es mas chico " + num1)
+//     }else if(num2 < num3 && num2 < num1){
+//     console.log("Es mas chico " + num2)
+//     }else{
+//         console.log("Es mas chico " + num3)
+//     }
+// }
+
+// esMasChico(3,4,5)
+// esMasChico(8,5,10)
+// esMasChico(5,4,2)
+
+//Ejercicio 5
